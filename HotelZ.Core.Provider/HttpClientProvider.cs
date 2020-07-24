0722e3f7a03c89d504c515dbcad5ed4f9149e18b
Phat <phat.thanhnguyen@topicus.nl>
2020-07-24 16:41:04 +0700

    Restructure

---
+++ HotelZ.Core.Provider/HttpClientProvider.cs
@@ -0,0 +1,19 @@
+﻿using HotelZ.Core.Provider.Interfaces;
+using HotelZ.Core.Provider.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HotelZ.Core.Provider
+{
+    public class HttpClientProvider : IHttpClientProvider
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpClientProvider(IHttpClientFactory httpClientFactory)
+        {
+            this._httpClient = httpClientFactory.CreateClient();
+        }
+
+        
+    }
+}
---
+++ HotelZ.Core.Provider/Models/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelZ.Core.Provider.Models
+{
+    public class ApiResponse<T> where T : class
+    {
+    }
+}
